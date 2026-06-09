ccac4acec9f06952a03aa07b58217d726848a671
FelipeRibeiroSalles <feliperscamargo@gmail.com>
2026-06-09 12:46:38 -0300

    Adiciona EnsureCreated

--- AgroSphere.Api/Program.cs
+++ AgroSphere.Api/Program.cs
@@ -19,6 +19,11 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     ));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 
